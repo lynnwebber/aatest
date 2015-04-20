e047c42e18fe1ee5ce1b6f8a803b642a47acf240
Lynn Webber <lynn@wellkeeper.com>
2015-04-20 11:00:05 -0600

    docs and tests

--- scheme/test.scm
+++ scheme/test.scm
@@ -4,3 +4,16 @@
   (display "howdy there world!\n"))
 
 (howdy)
+
+(display (string-append "1" "2"))
+(newline)
+
+(define my-line
+    (lambda (ln)
+        (display ln)
+        (newline)))
+
+(define inf (open-input-file "example.txt"))
+(define lns (read-lines inf))
+(for-each my-line lns)
+(close-input-port inf)
