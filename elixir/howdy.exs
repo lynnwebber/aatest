08bb023300e3eaf87c62a37532736c2a426d3b98
Lynn Webber <lynn@wellkeeper.com>
2015-05-03 21:43:09 -0600

    updates to how-to documents

---
+++ elixir/howdy.exs
@@ -0,0 +1,8 @@
+# the first test of elixir.  looking forward...
+IO.puts "howdy world\nhere comes some cowboy coding"
+IO.puts "lets check out some first class function stuff:"
+add_one = fn a -> a + 1 end
+
+IO.puts add_one.(5)
+
+# do some pattern matching and learn about case/do/end
