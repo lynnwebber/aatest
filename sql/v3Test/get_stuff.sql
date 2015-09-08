f8afc672b68e0359036cce2a2ab676cec820328f
Lynn Webber <lynn@wellkeeper.com>
2015-09-08 10:57:36 -0600

    updated sql for loading test data for alarms

--- sql/v3Test/get_stuff.sql
+++ sql/v3Test/get_stuff.sql
@@ -1,18 +1,15 @@
--- select nlnm.position,nlnm.notification_method_id, nlnm.delay, nm.contact_id, nm.type, nm.data, nc.name
--- from notification_lists_notification_methods nlnm
+-- for debugging
+-- select adnl.position as list_position,adnl.notification_list_id,nlnm.position as method_position,nlnm.notification_method_id,nm.contact_id, nc.name, nm.type, nm.data, nlnm.delay
+--
+-- --select nc.name, nm.type, nm.data, nlnm.delay
+-- from alarm_definitions_notification_lists adnl
+-- join notification_lists_notification_methods nlnm on (adnl.notification_list_id=nlnm.notification_list_id)
 -- join notification_methods nm on (nlnm.notification_method_id=nm.notification_method_id)
 -- join notification_contacts nc on (nm.contact_id=nc.contact_id)
--- where notification_list_id in (select notification_list_id
---     from alarm_definitions_notification_lists
---     where alarm_definition_id = 1101
---     order by position asc)
--- order by ;
+-- where alarm_definition_id = 1101
+-- order by adnl.position asc, nlnm.position;
 
-
-select adnl.notification_list_id, nlnm.position,nlnm.notification_method_id, nlnm.delay, nm.contact_id, nm.type, nm.data, nc.name
-from alarm_definitions_notification_lists adnl
-join notification_lists_notification_methods nlnm on (adnl.notification_list_id=nlnm.notification_list_id)
-join notification_methods nm on (nlnm.notification_method_id=nm.notification_method_id)
-join notification_contacts nc on (nm.contact_id=nc.contact_id)
-where alarm_definition_id = 1101
-order by adnl.position asc, nlnm.position;
+-- select operator_id, priority, description,
+--    short_message,full_message
+-- from alarm_definitions
+-- where alarm_definition_id = 1101;
--- sql/v3Test/load_contacts.sql
+++ sql/v3Test/load_contacts.sql
@@ -18,6 +18,21 @@ insert into notification_contacts
 values
  (1102,23,'todd flaska','active');
 
+insert into notification_contacts
+(contact_id,operator_id,name,status)
+values
+(1103,23,'walter farmer','active');
+
+insert into notification_contacts
+(contact_id,operator_id,name,status)
+values
+(1104,23,'buggs bunny','active');
+
+insert into notification_contacts
+(contact_id,operator_id,name,status)
+values
+(1105,23,'daffy duck','active');
+
 ;; ------------
 
 insert into notification_methods
@@ -28,12 +43,34 @@ values
 insert into notification_methods
  (notification_method_id,contact_id,type,data)
 values
+ (11002,1100,'email','lynn.webber@wellkeeper.com');
+
+
+insert into notification_methods
+ (notification_method_id,contact_id,type,data)
+values
  (11011,1101,'sms','5052581806');
 
 insert into notification_methods
  (notification_method_id,contact_id,type,data)
 values
+ (11012,1101,'email','randy.krall@wellkeeper.com');
+
+
+insert into notification_methods
+ (notification_method_id,contact_id,type,data)
+values
  (11021,1102,'email','todd.flaska@wellkeeper.com');
+
+insert into notification_methods
+ (notification_method_id,contact_id,type,data)
+values
+ (11022,1102,'sms','3036181769');
+
+insert into notification_methods
+ (notification_method_id,contact_id,type,data)
+values
+ (11031,1103,'email','walter.farmer@wellkeeper.com');
 
 ;; ----------------
 
@@ -46,6 +83,12 @@ insert into notification_lists
  (notification_list_id,operator_id,description)
 values
  (102,23,'Team Managers');
+
+insert into notification_lists
+ (notification_list_id,operator_id,description)
+values
+ (103,23,'FSO');
+
 
 ;; ------------------
 
@@ -63,3 +106,8 @@ insert into notification_lists_notification_methods
  (notification_list_id,notification_method_id,position)
 values
  (102,11021,1);
+
+insert into notification_lists_notification_methods
+ (notification_list_id,notification_method_id,position)
+values
+ (103,11031,0);
