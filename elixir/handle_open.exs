c40070b5dd41aa65e44e77e42dddcf9aeefe0404
Lynn Webber <lynn@wellkeeper.com>
2016-02-09 16:55:38 -0700

    updated with functions exercises

--- elixir/handle_open.exs
+++ elixir/handle_open.exs
@@ -4,3 +4,24 @@ handle_open = fn
 end
 IO.puts handle_open.(File.open("/Users/lynn/Desktop/rtu_channel_work.xml"))
 IO.puts handle_open.(File.open("whoknows.txt"))
+
+fb = fn
+    0, 0, _ -> "FizzBuzz"
+    0, _, _ -> "Fizz"
+    _, 0, _ -> "Buzz"
+    _, _, a -> a
+end
+
+xtest = fn
+    n -> fb.(rem(n,3),rem(n,5),n)
+end
+
+IO.puts xtest.(10)
+IO.puts xtest.(11)
+IO.puts xtest.(12)
+IO.puts xtest.(13)
+IO.puts xtest.(14)
+IO.puts xtest.(15)
+IO.puts xtest.(16)
+IO.puts xtest.(17)
+
